5d76a1ab583e625a022f4d1f09c855f6ae471c68
WebDevCF2m <gitweb@cf2m.be>
2025-02-07 16:07:13 +0100

    front controller ok

--- CF/public/index.php
+++ CF/public/index.php
@@ -5,5 +5,37 @@
 Front Controller  
 */
 
-// view homepage
-include "../template/homepageView.php";
+/*
+routing
+*/
+
+// La variable get 'p' existe
+if(isset($_GET['p'])){
+
+    // utilisation du switch
+    switch($_GET['p']){
+        // si nous sommes sur interview
+        case "interview" :
+            include "../template/interviewView.php";
+            break;
+        // si nous sommes sur exposition
+        case "expo" :
+            include "../template/expositionView.php";
+            break;
+        // si nous sommes sur conclusion
+        case "conclusion" :
+            include "../template/conclusionView.php";
+            break;
+        // si la variable get ne correspond pas
+        default :
+            include "../template/error404View.php";
+    }
+// pas de variable p, nous sommes sur l'accueil    
+}else{
+    // view homepage
+    include "../template/homepageView.php";
+}
+
+
+// déboggage variable get
+// var_dump($_GET);
